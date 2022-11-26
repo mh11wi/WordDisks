5f5157f42c4330e2b05545eca6576355efd932ce
Madeleine Hill <mh11wi@outlook.com>
2022-11-25 21:47:12 -0500

    Update jest config Test was previously failing after react-disks import.

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders the Word Disks app bar', () => {
   render(<App />);
   const element = screen.getByText(/Word Disks/i);
   expect(element).toBeInTheDocument();
