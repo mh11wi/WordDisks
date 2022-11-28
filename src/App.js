d781c8d21486d85fdf1478b99a8388465cd33023
Madeleine Hill <mh11wi@outlook.com>
2022-11-28 12:58:15 -0500

    Generate random words for disks Imported a words list and grabbed n words from the list with length m (controlled by state).

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import teal from '@mui/material/colors/teal';
 import AppBar from '@mui/material/AppBar';
@@ -8,26 +9,51 @@ import Button from '@mui/material/Button';
 import ReactDisks from 'react-disks';
 import './App.css';
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: teal[500],
+      light: teal[100],
+      dark: teal[800]
+    }
+  },
+});
+
+function generateRandomWords(wordsList, numberOfWords, lettersPerWord) {
+  const words = new Set();
+  while (words.size < numberOfWords) {
+    const word = wordsList[Math.floor(Math.random() * wordsList.length)];
+    if (word.length === lettersPerWord) {
+      words.add(word);
+    }
+  }
+  return Array.from(words);
+}
+
+function transpose(matrix) {
+  return matrix[0].map((col, i) => matrix.map(row => row[i]));
+}
+
 function App() {
-  const disksText = [
-    ['a', 'b', 'c', 'd'], 
-    ['e', 'f', 'g', 'h'], 
-    ['i', 'j', 'k', 'l'], 
-    ['m', 'n', 'o', 'p'], 
-    ['q', 'r', 's', 't'], 
-    ['u', 'v', 'w', 'x'], 
-    ['y', 'z', '1', '2']
-  ];
+  const [wordsList, setWordsList] = useState(null);
+  const [disksText, setDisksText] = useState(null);
+  const [numberOfDisks, setNumberOfDisks] = useState(7);
+  const [lettersPerDisk, setLettersPerDisk] = useState(8);
   
-  const theme = createTheme({
-    palette: {
-      primary: {
-        main: teal[500],
-        light: teal[100],
-        dark: teal[800]
+  useEffect(() => {
+    const words = require('random-words');
+    setWordsList(words.wordList);
+  }, []);
+  
+  useEffect(() => {
+    if (wordsList) {
+      const words = generateRandomWords(wordsList, lettersPerDisk, numberOfDisks);
+      for (let i = 0; i < words.length; i++) {
+        words[i] = words[i].split('');
       }
-    },
-  });
+      setDisksText(transpose(words));
+    }
+  }, [wordsList, numberOfDisks, lettersPerDisk]);
   
   return (
     <div className="App">
